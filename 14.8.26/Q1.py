d9879dbca89321e64c1fec9ed1949580d8a124f3
CrazyWizrd <ojasch457@gmail.com>
2026-08-14 19:27:54 +0530

    Day14.8.26

---
+++ 14.8.26/Q1.py
@@ -0,0 +1,9 @@
+# Combine two dicts, adding values for common keys.
+d1 = {"a":5, "b":3}
+d2 = {"b":4, "c":2}
+for i in d2:
+    if i in d1.keys():
+        d1[i] += d2[i]
+    else:
+        d1[i] = d2[i]
+print(d1)
